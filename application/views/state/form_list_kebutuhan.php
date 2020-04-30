1eaa9edccde7aa30cddad1392c2f962c7b58aa22
olwennnn <j.olwennnn@users.noreply.github.com>
2020-04-30 19:06:51 +0700

    update form list

--- application/views/state/form_list_kebutuhan.php
+++ application/views/state/form_list_kebutuhan.php
@@ -5,20 +5,22 @@
 			<input type="text" class="form-control" id="nama_ukm" name="nama_ukm">
 			<?= form_error('nama_ukm', '<small class="form-text text-danger">', '</small>'); ?>
 		</div>
-		<div class="form-group">
-			<label for="nama_pj">Nama Penanggungjawab:</label>
-            <input type="text" class="form-control" id="nama_pj" name="nama_pj">
-            <?= form_error('nama_pj', '<small class="form-text text-danger">', '</small>'); ?>
-		</div>
-		<div class="form-group">
-			<label for="contact_pj">Contact Penanggungjawab (no telp):</label>
-            <input type="text" class="form-control" id="contact_pj" name="contact_pj">
-            <?= form_error('contact_pj', '<small class="form-text text-danger">', '</small>'); ?>
+		<div class="form-group row">
+			<div class="col-sm-7">
+				<label for="nama_pj">Nama Penanggungjawab:</label>
+				<input type="text" class="form-control" id="nama_pj" name="nama_pj">
+				<?= form_error('nama_pj', '<small class="form-text text-danger">', '</small>'); ?>
+			</div>
+			<div class="col-sm-5">
+				<label for="contact_pj">Contact Penanggungjawab (no telp):</label>
+				<input type="text" class="form-control" id="contact_pj" name="contact_pj">
+				<?= form_error('contact_pj', '<small class="form-text text-danger">', '</small>'); ?>
+			</div>
 		</div>
 		<div class="form-group">
 			<label for="deskripsi_kegiatan">Kegiatan yang akan dilakukan:</label>
-            <input type="text" class="form-control" id="deskripsi_kegiatan" name="deskripsi_kegiatan">
-            <?= form_error('nama_ukm', '<small class="form-text text-danger">', '</small>'); ?>
+			<textarea type="text" class="form-control" style="resize:none;" id="deskripsi_kegiatan" name="deskripsi_kegiatan" rows="5"></textarea>
+			<?= form_error('nama_ukm', '<small class="form-text text-danger">', '</small>'); ?>
 		</div>
 		<div class="form-group">
 			<label for="waktu">Waktu yang dibutuhkan untuk melakukan kegiatan:</label>
@@ -34,42 +36,46 @@
 		</div>
 		<hr>
 		<h3>Perlengkapan yang dibutuhkan oleh UKM/Media Kampus</h3>
-		<div class="form-group">
-			<label for="jml_meja">Meja</label>
-			<input type="number" class="form-control" id="jml_meja" name="jml_meja">
+		<div class="form-group row">
+			<div class="col-sm-3">
+				<label for="jml_meja">Meja</label>
+				<input type="number" class="form-control" id="jml_meja" name="jml_meja">
+			</div>
+			<div class="col-sm-3">
+				<label for="jml_kursi">Kursi</label>
+				<input type="number" class="form-control" id="jml_kursi" name="jml_kursi">
+			</div>
+			<div class="col-sm-3">
+				<label for="jml_kabelroll">Kabel Roll</label>
+				<input type="number" class="form-control" id="jml_kabelroll" name="jml_kabelroll">
+			</div>
+			<div class="col-sm-3">
+				<label for="jml_mic">Microphone</label>
+				<input type="number" class="form-control" id="jml_mic" name="jml_mic">
+			</div>
 		</div>
-		<div class="form-group">
-			<label for="jml_kursi">Kursi</label>
-			<input type="number" class="form-control" id="jml_kursi" name="jml_kursi">
-		</div>
-		<div class="form-group">
-			<label for="jml_kabelroll">Kabel Roll</label>
-			<input type="number" class="form-control" id="jml_kabelroll" name="jml_kabelroll">
-		</div>
-		<div class="form-group">
-			<label for="jml_mic">Microphone</label>
-			<input type="number" class="form-control" id="jml_mic" name="jml_mic">
-		</div>
-		<div class="form-group">
-			<label for="jml_soundsystem">Sound System</label>
-			<input type="number" class="form-control" id="jml_mic" name="jml_soundsystem">
-		</div>
-		<div class="form-group">
-			<label for="jml_layar">Layar</label>
-			<input type="number" class="form-control" id="jml_layar" name="jml_layar">
-		</div>
-		<div class="form-group">
-			<label for="jml_proyektor">Proyektor</label>
-			<input type="number" class="form-control" id="jml_proyektor" name="jml_proyektor">
-		</div>
-		<div class="form-group">
-			<label for="jml_pc">PC</label>
-			<input type="number" class="form-control" id="jml_pc" name="jml_pc">
+		<div class="form-group row">
+			<div class="col-sm-3">
+				<label for="jml_soundsystem">Sound System</label>
+				<input type="number" class="form-control" id="jml_mic" name="jml_soundsystem">
+			</div>
+			<div class="col-sm-3">
+				<label for="jml_layar">Layar</label>
+				<input type="number" class="form-control" id="jml_layar" name="jml_layar">
+			</div>
+			<div class="col-sm-3">
+				<label for="jml_proyektor">Proyektor</label>
+				<input type="number" class="form-control" id="jml_proyektor" name="jml_proyektor">
+			</div>
+			<div class="col-sm-3">
+				<label for="jml_pc">PC</label>
+				<input type="number" class="form-control" id="jml_pc" name="jml_pc">
+			</div>
 		</div>
 		<hr>
 		<div class="form-group">
 			<label for="perlengkapan_peserta">Perlengkapan Peserta</label>
-			<input type="text" class="form-control" id="perlengkapan_peserta" name="perlengkapan_peserta">
+			<textarea type="text" class="form-control" style="resize:none;" id="perlengkapan_peserta" name="perlengkapan_peserta" rows="5"></textarea>
 		</div>
 		<button class="btn btn-block btn-primary my-3" name="submit">Submit</button>
 	</form>
