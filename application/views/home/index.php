67f4dd1431ae49f11005c95a1e68a21cce8b0c56
olwennnn <j.olwennnn@users.noreply.github.com>
2020-05-09 22:53:46 +0700

    update on home and responsive

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,7 +1,17 @@
-<div class="jumbotron">
-  <h1 class="display-4">Form List Kebutuhan STATE</h1>
-  <!--<p class="lead">Teks 1</p>
-  <hr class="my-4">
-  <p>Teks 2</p>-->
-  <a class="btn btn-primary btn-lg mt-3" href="<?= base_url('state/formListKebutuhan'); ?>" role="button">Klik disini</a>
+<div id="firstcont" class="mxm-cont">
+  <div class="mxm-cont-second">
+    <img alt="Web Studio" id="mxm-illus" src="<?= base_url('assets/bg/form_illus.svg'); ?>" />
+    <div>
+      <h2>FORM LIST KEBUTUHAN<br> STATE 2020</h2>
+      <div class="d-flex align-center justify-content-center mt-4">
+        <button class="mxm-btn-yellow">CLICK HERE</button>
+      </div>
+    </div>
+  </div>
 </div>
+<div>
+  <svg class="mxm-wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#F4224B" fill-opacity="1" d="M0,160L48,165.3C96,171,192,181,288,165.3C384,149,480,107,576,101.3C672,96,768,128,864,149.3C960,171,1056,181,1152,197.3C1248,213,1344,235,1392,245.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+  <svg class="pt-0 mt-0" style="position:absolute;bottom:0;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#41CEBA" fill-opacity="1" d="M0,192L48,170.7C96,149,192,107,288,101.3C384,96,480,128,576,165.3C672,203,768,245,864,229.3C960,213,1056,139,1152,128C1248,117,1344,171,1392,197.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+  <svg class="pt-0 mt-0" style="position:absolute;bottom:0;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFD008" fill-opacity="1" d="M0,192L48,202.7C96,213,192,235,288,240C384,245,480,235,576,234.7C672,235,768,245,864,229.3C960,213,1056,171,1152,160C1248,149,1344,171,1392,181.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+</div>
+
--- application/views/state/form_list_kebutuhan.php
+++ application/views/state/form_list_kebutuhan.php
@@ -1,86 +1,153 @@
-<div class="container w-75 my-5" style="font-family: 'canaro-medium';">
-<?= $this->session->flashdata('message'); ?>
-	<form action="formListKebutuhan" method="POST" id="list-kebutuhan-form" class="pt-3">
-		<div class="form-group">
-			<label for="nama_ukm">Nama UKM/Media Kampus</label>
-			<input type="text" class="form-control" id="nama_ukm" name="nama_ukm" value="<?= set_value('nama_ukm'); ?>">
-			<?= form_error('nama_ukm', '<small class="form-text text-danger">', '</small>'); ?>
-		</div>
-		<div class="form-group row">
-			<div class="col-sm-7">
-				<label for="nama_pj">Nama Penanggungjawab:</label>
-				<input type="text" class="form-control" id="nama_pj" name="nama_pj" value="<?= set_value('nama_pj'); ?>">
-				<?= form_error('nama_pj', '<small class="form-text text-danger">', '</small>'); ?>
+<div>
+	<svg class="mxm-wave-top" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#F4224B" fill-opacity="1" d="M0,128L48,160C96,192,192,256,288,266.7C384,277,480,235,576,213.3C672,192,768,192,864,197.3C960,203,1056,213,1152,202.7C1248,192,1344,160,1392,144L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+</div>
+<div id="form-list-state" class="container-fluid">
+	<div class="container">
+		<h1 id="title-head">Form List Kebutuhan<br>STATE 2020</h1>
+	</div>
+	<div id="form-container" class="container pt-3 pb-3 rounded">
+		<form action="formListKebutuhan" method="POST" id="list-kebutuhan-form">
+			<div class="form-group">
+				<label for="nama_ukm">Nama UKM/Media Kampus</label>
+				<select name="nama_ukm" id="nama_ukm" class="form-control">
+				<?php
+				$ukm_olahraga = array(
+					"Lions Basket",
+					"Lions Futsal",
+					"Capoeira",
+					"Lions Badminton",
+					"Ultimatum Taekwondo",
+					"Tenis Meja",
+					"Lions Voli",
+					"Spectre",
+					"Fortius"
+				);
+
+				$ukm_seni = array(
+					"Ultima Sonora",
+					"Teater Katak",
+					"Tracce",
+					"J-Cafe",
+					"Qorie",
+					"Obscure",
+					"Street Dance",
+					"UMN Symphony Orchestra (USO)",
+					"Ultima Toys",
+					"Mufomic"
+				);
+
+				$ukm_sains = array(
+					"Rencang",
+					"Mapala UMN",
+					"Game Development Club",
+					"UESC",
+					"UMN Programming Club"
+				);
+
+				$media_kampus = array(
+					"UMN TV",
+					"UMN Radio",
+					"ULTIMAGZ",
+					"UMN Juice",
+					"Masterpiece"
+				);
+				?>
+				<?php 
+				echo "<option value='' disabled>-- UKM Olahraga --</option>";
+				foreach($ukm_olahraga as $ukm1){ 
+					echo "<option value='$ukm1'>$ukm1</option>";
+				}; 
+				echo "<option value='' disabled>-- UKM Seni dan Budaya --</option>";
+				foreach($ukm_seni as $ukm2){ 
+					echo "<option value='$ukm2'>$ukm2</option>";
+				}; 
+				echo "<option value='' disabled>-- UKM Sains dan Sosial --</option>";
+				foreach($ukm_sains as $ukm3){ 
+					echo "<option value='$ukm3'>$ukm3</option>";
+				}; 
+				echo "<option value='' disabled>-- Media Kampus --</option>";
+				foreach($media_kampus as $ukm4){ 
+					echo "<option value='$ukm4'>$ukm4</option>";
+				}; 
+				?>
+				<option value='' disabled>-- Kegiatan Kemahasiswaan & Lembaga Seni Otonom --</option>
+				<option value="Skystar Ventures">Skystar Ventures</option>
+				</select>
+				<?= form_error('nama_ukm', '<small class="form-text text-danger">', '</small>'); ?>
 			</div>
-			<div class="col-sm-5">
-				<label for="contact_pj">Contact Penanggungjawab (no telp):</label>
-				<input type="text" class="form-control" id="contact_pj" name="contact_pj" value="<?= set_value('contact_pj'); ?>">
-				<?= form_error('contact_pj', '<small class="form-text text-danger">', '</small>'); ?>
+			<div class="form-row">
+				<div class=" form-group col-sm-7">
+					<label for="nama_pj">Nama Penanggungjawab:</label>
+					<input type="text" class="form-control" id="nama_pj" name="nama_pj">
+					<?= form_error('nama_pj', '<small class="form-text text-danger">', '</small>'); ?>
+				</div>
+				<div class="form-group col-sm-5">
+					<label for="contact_pj">Contact Penanggungjawab (no telp):</label>
+					<input type="text" class="form-control" id="contact_pj" name="contact_pj">
+					<?= form_error('contact_pj', '<small class="form-text text-danger">', '</small>'); ?>
+				</div>
 			</div>
-		</div>
-		<div class="form-group">
-			<label for="deskripsi_kegiatan">Kegiatan yang akan dilakukan:</label>
-			<textarea type="text" class="form-control" style="resize:none;" id="deskripsi_kegiatan" name="deskripsi_kegiatan" rows="5"><?= set_value('deskripsi_kegiatan'); ?></textarea>
-			<?= form_error('deskripsi_kegiatan', '<small class="form-text text-danger">', '</small>'); ?>
-		</div>
-		<div class="form-group">
-			<label for="waktu">Waktu yang dibutuhkan untuk melakukan kegiatan (dalam jam):</label>
-			<input type="number" class="form-control" id="waktu" name="waktu" min="1.5" max="3" step="0.5">
-			<?= form_error('waktu', '<small class="form-text text-danger">', '</small>'); ?>
-		</div>
-		<div class="form-group">
-			<label for="ruangan">Ruangan yang biasa digunakan UKM/Media Kampus:</label>
-			<input type="text" class="form-control" id="ruangan" name="ruangan" value="<?= set_value('ruangan'); ?>">
-		</div>
-		<div class="form-group">
-			<label for="kuota">Jumlah kuota peserta (bila ada):</label>
-			<input type="number" class="form-control" id="kuota" name="kuota" value="<?= set_value('ruangan'); ?>">
-		</div>
-		<hr>
-		<h3>Perlengkapan yang dibutuhkan oleh UKM/Media Kampus:</h3>
-		<h6 class="pb-2">Jumlah peminjaman barang diberikan limit karena bergantung pada ketersediaan yang ada.</h6>
-		<div class="form-group row">
-			<div class="col-sm-3">
-				<label for="jml_meja">Meja</label>
-				<input type="number" class="form-control" id="jml_meja" name="jml_meja" min="0" max="4" value="0">
+			<div class="form-group">
+				<label for="deskripsi_kegiatan">Kegiatan yang akan dilakukan:</label>
+				<textarea type="text" class="form-control" style="resize:none;" id="deskripsi_kegiatan" name="deskripsi_kegiatan" rows="5"></textarea>
+				<?= form_error('nama_ukm', '<small class="form-text text-danger">', '</small>'); ?>
 			</div>
-			<div class="col-sm-3">
-				<label for="jml_kursi">Kursi</label>
-				<input type="number" class="form-control" id="jml_kursi" name="jml_kursi" min="0" max="999" value="0">
+			<div class="form-group">
+				<label for="waktu">Waktu yang dibutuhkan untuk melakukan kegiatan:</label>
+				<input type="number" class="form-control" id="waktu" name="waktu">
 			</div>
-			<div class="col-sm-3">
-				<label for="jml_kabelroll">Kabel Roll</label>
-				<input type="number" class="form-control" id="jml_kabelroll" name="jml_kabelroll" min="0" max="4" value="0">
+			<div class="form-group">
+				<label for="ruangan">Ruangan yang biasa digunakan UKM/Media Kampus:</label>
+				<input type="text" class="form-control" id="ruangan" name="ruangan">
 			</div>
-			<div class="col-sm-3">
-				<label for="jml_mic">Microphone</label>
-				<input type="number" class="form-control" id="jml_mic" name="jml_mic" min="0" max="2" value="0">
+			<div class="form-group">
+				<label for="kuota">Jumlah kuota peserta (bila ada):</label>
+				<input type="number" class="form-control" id="kuota" name="kuota">
 			</div>
-		</div>
-		<div class="form-group row">
-			<div class="col-sm-3">
-				<label for="jml_soundsystem">Sound System</label>
-				<input type="number" class="form-control" id="jml_mic" name="jml_soundsystem" min="0" max="2" value="0">
+			<hr>
+			<h3>Perlengkapan yang dibutuhkan oleh UKM/Media Kampus</h3>
+			<div class="form-row">
+				<div class="form-group col-sm-3">
+					<label for="jml_meja">Meja</label>
+					<input type="number" class="form-control" id="jml_meja" name="jml_meja">
+				</div>
+				<div class="form-group col-sm-3">
+					<label for="jml_kursi">Kursi</label>
+					<input type="number" class="form-control" id="jml_kursi" name="jml_kursi">
+				</div>
+				<div class="form-group col-sm-3">
+					<label for="jml_kabelroll">Kabel Roll</label>
+					<input type="number" class="form-control" id="jml_kabelroll" name="jml_kabelroll">
+				</div>
+				<div class="form-group col-sm-3">
+					<label for="jml_mic">Microphone</label>
+					<input type="number" class="form-control" id="jml_mic" name="jml_mic">
+				</div>
 			</div>
-			<div class="col-sm-3">
-				<label for="jml_layar">Layar</label>
-				<input type="number" class="form-control" id="jml_layar" name="jml_layar" min="0" max="2" value="0">
+			<div class="form-row">
+				<div class="form-group col-sm-3">
+					<label for="jml_soundsystem">Sound System</label>
+					<input type="number" class="form-control" id="jml_mic" name="jml_soundsystem">
+				</div>
+				<div class="form-group col-sm-3">
+					<label for="jml_layar">Layar</label>
+					<input type="number" class="form-control" id="jml_layar" name="jml_layar">
+				</div>
+				<div class="form-group col-sm-3">
+					<label for="jml_proyektor">Proyektor</label>
+					<input type="number" class="form-control" id="jml_proyektor" name="jml_proyektor">
+				</div>
+				<div class="form-group col-sm-3">
+					<label for="jml_pc">PC</label>
+					<input type="number" class="form-control" id="jml_pc" name="jml_pc">
+				</div>
 			</div>
-			<div class="col-sm-3">
-				<label for="jml_proyektor">Proyektor</label>
-				<input type="number" class="form-control" id="jml_proyektor" name="jml_proyektor" min="0" max="2" value="0">
+			<hr class="mt-1">
+			<div class="form-group">
+				<label for="perlengkapan_peserta">Perlengkapan Peserta</label>
+				<textarea type="text" class="form-control" style="resize:none;" id="perlengkapan_peserta" name="perlengkapan_peserta" rows="5"></textarea>
 			</div>
-			<div class="col-sm-3">
-				<label for="jml_pc">PC</label>
-				<input type="number" class="form-control" id="jml_pc" name="jml_pc" min="0" max="2" value="0">
-			</div>
-		</div>
-		<hr>
-		<div class="form-group">
-			<label for="perlengkapan_peserta">Perlengkapan yang perlu dibawa oleh peserta STATE 2020 (pakaian, peralatan, dan perlengkapan):</label>
-			<textarea type="text" class="form-control" style="resize:none;" id="perlengkapan_peserta" name="perlengkapan_peserta" rows="5"><?= set_value('perlengkapan_peserta'); ?></textarea>
-			<?= form_error('perlengkapan_peserta', '<small class="form-text text-danger">', '</small>'); ?>
-		</div>
-		<button class="btn btn-block btn-primary my-3" name="submit">Submit</button>
-	</form>
+			<button class="mxm-btn-yellow my-3 form-control" name="submit"><a>Submit</a></button>
+		</form>
+	</div>
 </div>
